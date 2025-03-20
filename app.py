5cc24325d9e22e5252fde6a7968461253c0e4549
Juan Manuel Hincapié <juanmahc0109@gmail.com>
2025-03-20 17:00:52 -0500

    Update greeting message and add Docker Compose configuration for CI automation

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hola, Docker pipeline funcionando 🚀'
+    return 'Hola, Docker pipeline funcionando 🚀, se hace CI automático?'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
