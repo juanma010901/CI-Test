7c6ecbbc5001963a1965004c73afe685293b956e
Juan Manuel Hincapié <juanmahc0109@gmail.com>
2025-03-20 18:40:10 -0500

    Add /info route to provide Docker pipeline status message

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ def hello():
     msg = f'Hola, Docker pipeline funcionando 🚀, se hace CI automático?'
     new_info = f'Automatic testing deployment'
     return msg + new_info
+
+@app.route('/info')
+def info():
+    msg = f'Hola, Docker pipeline funcionando 🚀, se hace CI automático'
+    return msg
     
 
 if __name__ == '__main__':
